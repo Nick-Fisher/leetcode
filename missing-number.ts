d49fc1b09dcc9fdb961befaf3e53d900f69c065a
Nick Fisher <manyyearsago@mail.ru>
2022-05-23 00:17:33 +0200

    missing number problem

---
+++ missing-number.ts
@@ -0,0 +1,24 @@
+/**
+ 
+Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array.
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
+2 is the missing number in the range since it does not appear in nums.
+
+*/
+
+function missingNumber(nums: number[]): number {
+    const n = nums.length;
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += nums[i];
+    }
+    return n * (n + 1) / 2 - sum;
+};
+
+missingNumber([3,0,1])
