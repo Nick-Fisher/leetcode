5f204a9843d07a7b8cd43d0bcc07e3ede9bd3165
Nick Fisher <manyyearsago@mail.ru>
2023-08-28 16:46:22 +0200

    solution for 150. Polish Notation

---
+++ src/Stack/150. Evaluate Reverse Polish Notation.ts
@@ -0,0 +1,67 @@
+/**
+ * 
+ * You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+ * 
+ */
+
+// Complexity: time O(n), space O(n)
+
+function evalRPN(tokens: string[]): number {
+  const operations = {
+    '*': (a: number, b: number) => a * b,
+    '/': (a: number, b: number) => Math.trunc(a / b), // | 0
+    '+': (a: number, b: number) => a + b,
+    '-': (a: number, b: number) => a - b,
+  };
+
+  const stack: (string | number)[] = [];
+
+  for (let i = 0; i < tokens.length; i++) {
+    if (operations[tokens[i]]) {
+      const secondOperand = stack.pop();
+      const firstOperand = stack.pop();
+      stack.push(operations[tokens[i]](firstOperand, secondOperand));
+    } else {
+      stack.push(Number(tokens[i]));
+    }
+  }
+
+  return stack.pop() as any;
+}
+
+console.log(evalRPN(['2', '1', '+', '3', '*'])); // 9
