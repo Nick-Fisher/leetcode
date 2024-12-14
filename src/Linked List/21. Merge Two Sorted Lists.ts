b44050ec6108de0b842b5cfeca64ff67375d9cc0
Nick Fisher <sincerelyyoursnikita@gmail.com>
2024-12-14 14:40:49 +0100

    21 merge 2 sorted linked list

---
+++ src/Linked List/21. Merge Two Sorted Lists.ts
@@ -0,0 +1,50 @@
+/**
+ * 
+ *      
+ * You are given the heads of two sorted linked lists list1 and list2.
+
+    Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+    Return the head of the merged linked list.
+
+    Input: list1 = [1,2,4], list2 = [1,3,4]
+    Output: [1,1,2,3,4,4]
+
+
+    // Complexity: time O(n), space O(1)
+ * 
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  let dummyNode = new ListNode();
+
+  let current = dummyNode;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  current.next = list1 || list2;
+
+  return dummyNode.next;
+}
