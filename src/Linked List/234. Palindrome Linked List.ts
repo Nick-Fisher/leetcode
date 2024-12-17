95ac35cd5145265a0b535834031ff3b45b5213b2
Nick Fisher <sincerelyyoursnikita@gmail.com>
2024-12-17 13:14:52 +0100

    234 palindrome linked list

---
+++ src/Linked List/234. Palindrome Linked List.ts
@@ -0,0 +1,54 @@
+/**
+ * 
+ *  Given the head of a singly linked list, return true if it is a 
+    palindrome
+    or false otherwise.
+ * 
+ *  Input: head = [1,2,2,1]
+    Output: true
+
+ *  Time Complexity: O(n), Space Complexity: O(1)
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function isPalindrome(head: ListNode | null): boolean {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow!.next;
+    fast = fast.next.next;
+  }
+
+  let prev = null;
+  let current = slow;
+
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  let first = head;
+  let second = prev;
+
+  while (second) {
+    if (first!.val !== second.val) {
+      return false;
+    }
+
+    first = first!.next;
+    second = second.next;
+  }
+
+  return true;
+}
