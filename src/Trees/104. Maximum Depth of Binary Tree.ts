2abe20888575b8b9f54bf1b029e009069751acdb
Nick Fisher <sincerelyyoursnikita@gmail.com>
2024-12-27 17:13:49 +0100

    104 max depth of binary tree

---
+++ src/Trees/104. Maximum Depth of Binary Tree.ts
@@ -0,0 +1,59 @@
+/**
+ * 
+ *  Given the root of a binary tree, return its maximum depth.
+
+    A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * Input: root = [3,9,20,null,null,15,7]
+    Output: 3
+
+    Time Complexity: O(n), space complexity: O(n)
+ * 
+ */
+
+// Recursive solution
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(
+    val: number = 0,
+    left: TreeNode | null = null,
+    right: TreeNode | null = null
+  ) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+function maxDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+
+// Iterative solution
+
+// Time complexity: O(n), space complexity: O(n)
+
+function maxDepthIterative(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  let stack = [{ node: root, depth: 1 }];
+  let result = 0;
+
+  while (stack.length) {
+    let elem = stack.pop();
+
+    result = Math.max(result, elem.depth);
+
+    elem.node.right &&
+      stack.push({ node: elem.node.right, depth: elem.depth + 1 });
+    elem.node.left &&
+      stack.push({ node: elem.node.left, depth: elem.depth + 1 });
+  }
+
+  return result;
+}
