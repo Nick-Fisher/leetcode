2b21201534615292ba7f3c7dad509400b3b36aea
Nick Fisher <sincerelyyoursnikita@gmail.com>
2026-02-12 13:09:35 +0100

    1179, 1484, 1587 SQL

---
+++ src/SQL/1484. Group Sold Products By The Date.sql
@@ -0,0 +1,24 @@
+-- Input: 
+-- Activities table:
+-- +------------+------------+
+-- | sell_date  | product     |
+-- +------------+------------+
+-- | 2020-05-30 | Headphone  |
+-- | 2020-06-01 | Pencil     |
+-- | 2020-06-02 | Mask       |
+-- | 2020-05-30 | Basketball |
+-- | 2020-06-01 | Bible      |
+-- | 2020-06-02 | Mask       |
+-- | 2020-05-30 | T-Shirt    |
+-- +------------+------------+
+-- Output: 
+-- +------------+----------+------------------------------+
+-- | sell_date  | num_sold | products                     |
+-- +------------+----------+------------------------------+
+-- | 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+-- | 2020-06-01 | 2        | Bible,Pencil                 |
+-- | 2020-06-02 | 1        | Mask                         |
+-- +------------+----------+------------------------------+
+
+SELECT sell_date as "sell_date", COUNT(DISTINCT product) as "num_sold", string_agg(DISTINCT product, ',') as "products"
+FROM Activities GROUP BY sell_date;
